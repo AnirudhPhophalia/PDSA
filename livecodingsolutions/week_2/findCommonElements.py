6eb97a1a156798da18e71c4fc3bd3d5e615d96f4
Anirudh Phophalia <anirudhphophalia@gmail.com>
2025-05-30 07:04:21 +0530

    added livecoding week2 solutions

---
+++ livecodingsolutions/week_2/findCommonElements.py
@@ -0,0 +1,5 @@
+# Write a Python function findCommonElements(L1, L2) that accepts two integer lists L1 and L2 of same length n and return a list that contains elements that are common to both lists. Write a efficient solution that runs in O(nlogn) or better time.
+
+# L1 contains all distinct integers and L2 contains all distinct integers, but there can be many elements common between L1 and L2.
+# Returned list contains all elements that are common to L1 and L2. The elements in the returned list can be in any order.
+
